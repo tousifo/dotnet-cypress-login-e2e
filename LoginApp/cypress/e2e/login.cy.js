287f533899bccf2bdc65cb969e73081bd27426e1
Tousif bin parves <tousifbinparves@stud.cou.ac.bd>
2025-06-30 00:30:55 +0600

    Add validation error test for empty login fields in login.cy.js

--- LoginApp/cypress/e2e/login.cy.js
+++ LoginApp/cypress/e2e/login.cy.js
@@ -6,4 +6,11 @@ describe('User Login Tests', () => {
     cy.get('input[name="Input.Password"]').should('exist')
     cy.get('button[type="submit"]').should('exist')
   })
+
+  it('should show validation errors for empty fields', () => {
+    cy.visit('http://localhost:5168/Identity/Account/Login')
+    cy.get('button[type="submit"]').click()
+    cy.contains('The Email field is required').should('exist')
+    cy.contains('The Password field is required').should('exist')
+  })
 })
